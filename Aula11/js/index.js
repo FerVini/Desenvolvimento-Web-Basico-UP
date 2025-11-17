669937153cfebe61e8f18643c834a49ed0503b7e
Fernando V. <111383455+FerVini@users.noreply.github.com>
2025-11-17 07:17:28 -0300

    Add files via upload

---
+++ Aula11/js/index.js
@@ -0,0 +1,29 @@
+function pesquisacep(valor) {
+    var cep = valor.replace(/\D/g, '');
+
+    if (cep != "") {
+        var validacep = /^[0-9]{8}$/;
+
+        if (validacep.test(cep)) {
+
+            document.querySelectorAll('.endereco').forEach(i => i.value = '...');
+
+            let script = document.createElement('script')
+            script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=meu_callback';
+            document.body.appendChild(script);
+        } else {
+            alert("CEP inválido!!");
+        }
+    }
+}
+
+function meu_callback(conteudo) {
+
+    if (!("erro" in conteudo)){
+        document.getElementById('rua').value = conteudo.logradouro;
+        document.getElementById('bairro').value = conteudo.bairro;
+        document.getElementById('cidade').value = conteudo.localidade;
+        document.getElementById('uf').value = conteudo.uf;
+        document.getElementById('ibge').value = conteudo.ibge;
+    }
+}
